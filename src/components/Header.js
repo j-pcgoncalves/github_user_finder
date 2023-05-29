1aafad044105b36d391bed3f2041343b9ba4fbcd
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-29 22:07:50 +0100

    create and style Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+            <div className="container-fluid">
+                <h1 className="mx-auto py-3">Github User Finder</h1>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
