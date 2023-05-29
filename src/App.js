a012b7ec2bce106c632db04be177798784288af8
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-29 23:33:02 +0100

    create Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Logic from "./components/Logic";
 import "./App.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Logic />
+      <Footer />
     </>
   );
 }
